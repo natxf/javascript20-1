f7377cbd323221940a8885f09fed03be5fd17ae0
vickykasprik <viktoriakasprik05@gmail.com>
2024-10-24 10:44:42 +0000

    cm3_1

--- script.js
+++ script.js
@@ -7,6 +7,7 @@
   const cw3 = document.getElementById("cw3");
   const cw4 = document.getElementById("cw4");
   const cw5 = document.getElementById("cw5");
+  const cw6 = document.getElementById("cw6");
   const answer = document.getElementById("answer");
 
   example.addEventListener("click", function () {
@@ -55,37 +56,52 @@
 
   cw3.addEventListener("click", function () {
     fetch("https://jsonplaceholder.typicode.com/posts/1")
-    .then((response) => response.json())
-    .then(post =>
-      {
+      .then((response) => response.json())
+      .then((post) => {
         console.log(post);
         answer.innerHTML = `<h1>${post.title}</h1>
         <p>${post.body}</p>`;
-      })
+      });
   });
   cw4.addEventListener("click", function () {
-    answer.innerHTML="Processing..."
-      fetch('https://jsonplaceholder.typicode.com/posts', {
-        method: 'POST',
-        body: JSON.stringify({
-          title: 'foo',
-          body: 'bar',
-          userId: 1,
-        }),
-        headers: {
-          'Content-type': 'application/json; charset=UTF-8',
-        },
-      })
-        .then((response) => response.json())
-        .then(post =>
-               {
-                 console.log(post);
-                 answer.innerHTML = `<h1>Dodano nowy post o id=${post.id}</h1>`
-               });
-} )
+    answer.innerHTML = "Processing...";
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: JSON.stringify({
+        title: "foo",
+        body: "bar",
+        userId: 1,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((post) => {
+        console.log(post);
+        answer.innerHTML = `<h1>Dodano nowy post o id=${post.id}</h1>`;
+      });
+  });
   cw5.addEventListener("click", function () {
-    window.alert("Loading...")
+    window.alert("Loading...");
     fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((posts) => {
+        console.log(posts);
+        let content = "<ul>";
+        posts.forEach((element) => {
+          content += `<li>
+                <h1>${element.title}</h1>
+                <p>${element.body}</p>
+                </li>`;
+        });
+        content += "</ul>";
+        answer.innerHTML = content;
+      });
+  });
+
+  cw6.addEventListener("click", function () {
+    fetch("https://my-json-server.typicode.com/trak2025zz/json-server/posts")
       .then((response) => response.json())
       .then((posts) => {
         console.log(posts);
